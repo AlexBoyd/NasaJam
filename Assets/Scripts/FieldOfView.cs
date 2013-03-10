8eb67c84b542e8b032dd5d3485acdb6ebf8a60aa
alex <alex@blackbirdinteractive.com>
2013-03-10 16:57:11 -0700

    got fov sorta working

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -6,10 +6,21 @@ public class FieldOfView : MonoBehaviour
 	public GameObject Player;
 	public GameObject Moon;
 	
+	public float MaxDistance;
+	public float MinDistance;
+	
+	
 	private void Update()
 	{
-		Camera.mainCamera.transform.position = Player.transform.position + new Vector3(0,0, -(100 + Mathf.Sqrt((Player.transform.position - Moon.transform.position).sqrMagnitude)));
+		Debug.Log(LogisticalDistance());
+		Debug.Log((Player.transform.position - Moon.transform.position).sqrMagnitude);
+		Camera.mainCamera.transform.position = Player.transform.position + new Vector3(0,0, 100 + 20000 * LogisticalDistance());
 		Player.transform.localScale = Vector3.one *  Mathf.Pow(Mathf.Abs(Camera.mainCamera.transform.position.z/100),0.8f);
+	}
+	
+	private float LogisticalDistance()
+	{
+		return 0.5f - (1/(1 + Mathf.Exp((Player.transform.position - Moon.transform.position).sqrMagnitude/6000000)));
 	}
 	
 	
