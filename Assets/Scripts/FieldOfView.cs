eb69626a7dfc586ad042a66c86168f64e7f5dcc4
alex <alex@blackbirdinteractive.com>
2013-03-10 17:33:12 -0700

    new camera zooming functionality

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -4,23 +4,27 @@ using System.Collections;
 public class FieldOfView : MonoBehaviour 
 {
 	public GameObject Player;
-	public GameObject Moon;
+	public GameObject Planet;
+	
+	public float VoyagerSize = 1;
 	
 	public float MaxDistance;
 	public float MinDistance;
 	
 	
-	private void Update()
+	
+	
+	private void FixedUpdate()
 	{
 		Debug.Log(LogisticalDistance());
-		Debug.Log((Player.transform.position - Moon.transform.position).sqrMagnitude);
-		Camera.mainCamera.transform.position = Player.transform.position + new Vector3(0,0, 100 + 20000 * LogisticalDistance());
-		Player.transform.localScale = Vector3.one *  Mathf.Pow(Mathf.Abs(Camera.mainCamera.transform.position.z/100),0.8f);
+		Debug.Log((Player.transform.position - Planet.transform.position).sqrMagnitude);
+		Camera.mainCamera.transform.position = Player.transform.position + new Vector3(0,0, Mathf.Clamp(20000 * LogisticalDistance(), MinDistance, MaxDistance));
+		Player.transform.localScale = Vector3.one * VoyagerSize * Mathf.Pow(Mathf.Abs(Camera.mainCamera.transform.position.z/100),0.8f);
 	}
 	
 	private float LogisticalDistance()
 	{
-		return 0.5f - (1/(1 + Mathf.Exp((Player.transform.position - Moon.transform.position).sqrMagnitude/6000000)));
+		return 0.5f - (1/(1 + Mathf.Exp(Mathf.Pow((Player.transform.position - Planet.transform.position).sqrMagnitude/6000000, 0.8f))));
 	}
 	
 	
